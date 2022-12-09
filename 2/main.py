b47c1b41217934ff65515ea37b26597060a8469d
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2022-12-09 23:05:32 +0900

    edit main.py tts message

--- 2/main.py
+++ 2/main.py
@@ -19,17 +19,17 @@ def main():
         tts.direct_tts("네")
         command = stt.get_text(timeout=5, phrase=5)
         if command is None:
-            tts.direct_tts("잘 못알17아들었어요")
+            tts.direct_tts("잘 못알아들었어요")
             continue
         print(f"인식된 텍스트: {command}")
         if "유튜브" in command:
             if "재생" in command or "틀어" in command:
-                thr = tts.direct_tts("유튜브에서 오디오를 재생합니다.", wait=False)
+                thr = tts.direct_tts("노래를 재생합니다.", wait=False)
                 youtube.set_with_text("음악")
                 thr.join()
                 
             elif "중지" in command or "꺼" in command:
-                tts.direct_tts("오디오를 종료합니다.")
+                tts.direct_tts("노래를 중지합니다.")
                 youtube.stop()
         elif "알람" in command:
             Alarming().set_alarm()
